6f81f626d9a118c84f72eb9e8c9345140ff9f40a
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-05-08 19:38:31 +0200

    possibly last mistake fixed

--- src/flags/flag_-r.c
+++ src/flags/flag_-r.c
@@ -1,36 +1,36 @@
 #include "../../includes/ls.h"
 
 void print_directory_contents_recursively(t_flags *flags, const char *dir_path,
-                                          int folder_count) {
-  DIR *dir = opendir(dir_path);
+																					int folder_count) {
+	DIR *dir = opendir(dir_path);
 
-  if (dir == NULL)
-    return perror("Unable to open directory");
+	if (dir == NULL)
+		return perror("Unable to open directory");
 
-  struct dirent *entry;
-  while ((entry = readdir(dir)) != NULL) {
-    struct stat path_stat;
-    stat(entry->d_name, &path_stat);
+	struct dirent *entry = NULL;
+	while ((entry = readdir(dir)) != NULL) {
+		struct stat path_stat = {0};
+		stat(entry->d_name, &path_stat);
 
-    if (S_ISDIR(path_stat.st_mode) && ft_strncmp(entry->d_name, ".", 1) != 0 &&
-        ft_strncmp(entry->d_name, "..", 2) != 0) {
-      char path[1024];
+		if (S_ISDIR(path_stat.st_mode) && ft_strncmp(entry->d_name, ".", 1) != 0 &&
+				ft_strncmp(entry->d_name, "..", 2) != 0) {
+			char path[1024] = "";
 
-      construct_path(path, dir_path, entry->d_name);
+			construct_path(path, dir_path, entry->d_name);
 
-      struct stat path_stat_check;
-      stat(path, &path_stat_check);
+			struct stat path_stat_check = {0};
+			stat(path, &path_stat_check);
 
-      if (S_ISDIR(path_stat_check.st_mode)) {
-        write(1, "\n", 1);
-        write(1, path, ft_strlen(path));
-        write(1, ":\n", 2);
-        ls_with_flags(flags, path, folder_count);
-        print_directory_contents_recursively(flags, path, folder_count);
-      }
-    }
-  }
-  closedir(dir);
+			if (S_ISDIR(path_stat_check.st_mode)) {
+				write(1, "\n", 1);
+				write(1, path, ft_strlen(path));
+				write(1, ":\n", 2);
+				ls_with_flags(flags, path, folder_count);
+				print_directory_contents_recursively(flags, path, folder_count);
+			}
+		}
+	}
+	closedir(dir);
 }
 
 /*
